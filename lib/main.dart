8267aede027c78dc48c37ee069f0c358c0fda299
nikhil-stack <56017618+nikhil-stack@users.noreply.github.com>
2022-11-09 00:38:20 +0530

    readme added

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:provider/provider.dart';
 import 'package:socialx/services/auth_services.dart';
 import 'package:socialx/screens/home_page.dart';
 import 'package:socialx/screens/Tabs_page.dart';
